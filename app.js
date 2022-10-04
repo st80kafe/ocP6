21c71ac617dad1a86e549ee6e5e7b836d0adfda3
Peter Gianukos <st80kafe@gmail.com>
2022-10-03 23:00:06 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const mongoose = require('mongoose');
+const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
+const path = require('path');
+const mongo = process.env.DB_CONNECTION;
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+mongoose.connect(mongo)
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+module.exports = app;
